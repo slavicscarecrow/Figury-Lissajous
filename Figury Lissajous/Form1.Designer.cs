b2c7bfe160614906489dc60e951efd73f9866dd1
SlavicScarecrow <matylda.miedzinska@gmail.com>
2024-04-22 17:03:11 +0200

    Uploaded project

---
+++ Figury Lissajous/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace Figury_Lissajous
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar4 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar5 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar6 = new System.Windows.Forms.HScrollBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Wymaz = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.panel1.Location = new System.Drawing.Point(64, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(913, 913);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label1.Location = new System.Drawing.Point(1388, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Parametry osi Y";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(1249, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Częstotliwość";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(1249, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Przesunięcie fazowe";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(1249, 383);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Amplituda";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(1455, 174);
+            this.hScrollBar1.Minimum = 1;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(227, 40);
+            this.hScrollBar1.TabIndex = 5;
+            this.hScrollBar1.Value = 1;
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(1455, 274);
+            this.hScrollBar2.Maximum = 360;
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(227, 40);
+            this.hScrollBar2.TabIndex = 6;
+            this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar2_Scroll);
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.Location = new System.Drawing.Point(1455, 374);
+            this.hScrollBar3.Maximum = 400;
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(227, 40);
+            this.hScrollBar3.TabIndex = 7;
+            this.hScrollBar3.Value = 400;
+            // 
+            // hScrollBar4
+            // 
+            this.hScrollBar4.Location = new System.Drawing.Point(1455, 740);
+            this.hScrollBar4.Maximum = 400;
+            this.hScrollBar4.Name = "hScrollBar4";
+            this.hScrollBar4.Size = new System.Drawing.Size(227, 40);
+            this.hScrollBar4.TabIndex = 14;
+            this.hScrollBar4.Value = 400;
+            this.hScrollBar4.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar4_Scroll);
+            // 
+            // hScrollBar5
+            // 
+            this.hScrollBar5.Location = new System.Drawing.Point(1452, 640);
+            this.hScrollBar5.Maximum = 360;
+            this.hScrollBar5.Name = "hScrollBar5";
+            this.hScrollBar5.Size = new System.Drawing.Size(227, 40);
+            this.hScrollBar5.TabIndex = 13;
+            // 
+            // hScrollBar6
+            // 
+            this.hScrollBar6.LargeChange = 1;
+            this.hScrollBar6.Location = new System.Drawing.Point(1452, 540);
+            this.hScrollBar6.Minimum = 1;
+            this.hScrollBar6.Name = "hScrollBar6";
+            this.hScrollBar6.Size = new System.Drawing.Size(227, 40);
+            this.hScrollBar6.TabIndex = 12;
+            this.hScrollBar6.Value = 1;
+            this.hScrollBar6.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar6_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(1249, 749);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Amplituda";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(1249, 649);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Przesunięcie fazowe";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(1249, 549);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 21);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Częstotliwość";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Georgia", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label8.Location = new System.Drawing.Point(1388, 468);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(213, 32);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Parametry osi X";
+            // 
+            // Wymaz
+            // 
+            this.Wymaz.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.Wymaz.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Wymaz.Location = new System.Drawing.Point(1516, 893);
+            this.Wymaz.Name = "Wymaz";
+            this.Wymaz.Size = new System.Drawing.Size(289, 69);
+            this.Wymaz.TabIndex = 15;
+            this.Wymaz.Text = "Wymaż";
+            this.Wymaz.UseVisualStyleBackColor = false;
+            this.Wymaz.Click += new System.EventHandler(this.Wymaz_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2;
+            this.timer1.Tick += new System.EventHandler(this.Rysuj);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button1.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(1138, 893);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(289, 69);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Wymaz);
+            this.Controls.Add(this.hScrollBar4);
+            this.Controls.Add(this.hScrollBar5);
+            this.Controls.Add(this.hScrollBar6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.hScrollBar3);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.HScrollBar hScrollBar4;
+        private System.Windows.Forms.HScrollBar hScrollBar5;
+        private System.Windows.Forms.HScrollBar hScrollBar6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button Wymaz;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Figury Lissajous/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Figury_Lissajous
+{
+    public partial class Form1 : Form
+    {
+        static int x_center, y_center;
+        static int nowy_x, nowy_y;
+        static int stary_x, stary_y;
+        static int t = 0;
+
+        Pen mojePioro = new Pen(Color.MediumVioletRed);
+        Graphics g = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            x_center = panel1.Width / 2;
+            y_center = panel1.Height / 2;
+            stary_x = x_center;
+            stary_y = y_center;
+        }
+        void RysujPunkt()
+        {
+            Point[] points =
+            {
+                new Point(x_center, y_center),
+                new Point(0,0)
+            };
+            g.DrawLines(mojePioro, points);
+        }
+        double Sinus(int amplituda, int czestotliwosc, int przesuniecie, int czas)
+        {
+            return amplituda * Math.Sin(2 * Math.PI * czestotliwosc* 0.01 * czas + (przesuniecie*0.01745329252));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void Rysuj(object sender, EventArgs e)
+        {
+ 
+            nowy_x = (int)Sinus(hScrollBar4.Value, hScrollBar6.Value, hScrollBar5.Value, t)+x_center;
+            nowy_y = (int)Sinus(hScrollBar3.Value, hScrollBar1.Value, hScrollBar2.Value, t)+y_center;
+            Point[] points =
+           {
+                new Point(stary_x, stary_y),
+                new Point(nowy_x,nowy_y)
+            };
+            g.DrawLines(mojePioro, points);
+            stary_x = nowy_x;
+            stary_y = nowy_y;
+            t++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void hScrollBar6_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void hScrollBar4_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Wymaz_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            mojePioro.Width = 5;
+            g = panel1.CreateGraphics();
+        }
+    }
+}
